853111e8d99acac8a43c932631de62e8d816b42c
Mariska Archielli <mariska.arch@gmail.com>
2020-06-23 09:29:28 +0200

    training js

--- awesome_tshirt/__manifest__.py
+++ awesome_tshirt/__manifest__.py
@@ -11,11 +11,21 @@
     'category': 'Extra Tools',
     'version': '2.0',
     'application': True,
-    'depends': ['base', 'web'],
+    'depends': ['base', 'web', 'base_geolocalize'],
     'data': [
+        'assets.xml',
         'security/ir.model.access.csv',
         'views/awesome_tshirt_views.xml',
         'views/templates.xml',
     ],
-    'license': 'AGPL-3'
+    'license': 'AGPL-3',
+    'qweb': [
+        'static/src/xml/awesome_dashboard.xml',
+        'static/src/xml/form_view.xml',
+        'static/src/xml/home_menu.xml',
+        'static/src/xml/my_counter.xml',
+        'static/src/xml/order_kanban_view.xml',
+        'static/src/xml/quick_order_navigation.xml',
+        'static/src/xml/warning_widget.xml',
+    ]
 }
